e903f2ef2d369d40eec175dbb7869dc7f79a739a
7C93F3-L <206685181+7C93F3-L@users.noreply.github.com>
2026-08-21 05:21:00 +0800

    refactor: extract search capsule styles

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -8,6 +8,7 @@ import "./dialogs/ConfirmationPanels.css";
 import "./ColorPickerPopover.css";
 import "./results/ResultGrid.css";
 import "./results/ResultsView.css";
+import "./search/Cap7CESearchCapsule.css";
 import "./settings/SettingsSelect.css";
 import "./settings/ShortcutSettingsPanels.css";
 import "./settings/SkimDisplaySettingsRows.css";
