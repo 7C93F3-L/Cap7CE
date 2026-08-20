26513c730f744c14fec0c6dfe99502b49d80ec1c
7C93F3-L <206685181+7C93F3-L@users.noreply.github.com>
2026-08-21 05:39:33 +0800

    refactor: extract window control rail styles

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
+import "./WindowControlRail.css";
 import "./skim/SkimView.css";
 import "./preview/PreviewWindow.css";
 import "./dialogs/KeywordEditorCard.css";
